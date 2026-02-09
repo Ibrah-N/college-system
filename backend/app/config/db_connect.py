8e6dc899f458d3afb71bce9bd0669a053f79ef4d
Ibrah-N <ibrahnfreenlancer@gmail.com>
2026-02-09 16:00:18 +0500

    docker is added

--- backend/app/config/db_connect.py
+++ backend/app/config/db_connect.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://ibrahim@localhost/college_system"
+# -- local development --
+# DATABASE_URL = "postgresql://ibrahim@localhost/college_system"
+# engine = create_engine(DATABASE_URL)
 
+# -- env vars --
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+# -- docker development --
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
